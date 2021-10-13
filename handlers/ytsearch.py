3de9c4db359633b6ed56b3de9cda1a3ea5c05ce3
ylmzaksoy <81808120+ylmzaksoy@users.noreply.github.com>
2021-10-13 13:37:40 +0300

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -38,10 +38,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search bir bağımsız değişkene ihtiyaç duyar!")
+            await message.reply_text("/search Bir Bağımsız Değişkene İhtiyaç Duyar")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Aranıyor....")
+        m = await message.reply_text("Aranıyor...")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
